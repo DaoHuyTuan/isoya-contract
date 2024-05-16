6c0aaa3de381247a2b479d3345a490c898b91469
daohuytuan <tuandh95@gmail.com>
2024-05-16 15:40:43 +0700

    update

--- src/State/AccountState.sol
+++ src/State/AccountState.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.24;
 
 
 contract AccountState {
     address public owner;
-    uint256 total;
+    uint256 total = 0;
     struct Order {
+        uint256 id;
         string token;
         uint256 value;
         uint256 price;
@@ -21,26 +22,26 @@ contract AccountState {
         _;
     }
 
-    function create_order(uint256 _id, string memory _token, uint256 _value, uint256 _price) external payable  {
+    function create_order(string memory _token, uint256 _value, uint256 _price) external payable  {
         // require(msg.sender == owner, string(abi.encodePacked("Only contract owner can create")));
-        orders[_id] = Order(_token, _value, _price, true);
+        orders[total] = Order(total, _token, _value, _price, true);
         total++;    
     }
     
     function update_order(uint256 _id, Order memory _newOrder) external payable onlyOwner("update") {
-         require(orders[_id].existed, "Order does not exist");
-         orders[_id] = _newOrder;
+        require(orders[_id].existed, "Order does not exist");
+        orders[_id] = _newOrder;
     }
 
-    // function get_orders() external view returns (Order[] memory) {
-    //     Order[] memory _orders = new Order[](1);
-    //     for (uint256 i = 0; i < total; i++) {
-    //         _orders[i] = orders[i];
-    //     }
-    //     return _orders;
-    // }
+    function get_orders() external view returns (Order[] memory) {
+        Order[] memory _orders = new Order[](total);
+        for (uint256 i = 0; i < total; i++) {
+            _orders[i] = orders[i];
+        }
+        return _orders;
+    }
 
-    function test(uint256 _id) public view returns (Order memory) {
+    function get_order_by_id(uint256 _id) public view returns (Order memory) {
         return orders[_id];
     }
 }
